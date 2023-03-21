80ec6cbc1b0b1368f8db0fb84b0fd5263ddeafab
Haowei Sun <printfplus@outlook.com>
2023-03-20 23:30:27 -0400

    new clean data result attached

--- analysis_algorithm/Haowei/haowei_analysis.py
+++ analysis_algorithm/Haowei/haowei_analysis.py
@@ -1,14 +1,14 @@
 import pandas as pd
 from pathlib import Path
 import plotly.express as px
-
+import numpy as np
 
 haowei_path = Path('data/Haowei_cleaned.csv')
 dataset = pd.read_csv(haowei_path, index_col=0)
 
 # print the first 5 rows of the dataset
 print(dataset.head())
-
+dataset.replace(r'^\s*$', np.nan, regex=True, inplace=True)
 fig = px.histogram(dataset, x="AGE")
 # save the figure as a png file
 fig.write_image('fig1.png')
--- data_process/Haowei/data_process.py
+++ data_process/Haowei/data_process.py
@@ -1,14 +1,20 @@
 import pandas as pd
 from pathlib import Path
-
+import numpy as np
 
 
 haowei_path = Path('data/Haowei.csv')
 dataset = pd.read_csv(haowei_path, index_col=0)
-dataset = dataset.dropna()
+print(dataset.head())
+# replace all elements with whitespace cells with NaN
+dataset.replace(r'^\s*$', np.nan, regex=True, inplace=True)
+dataset_clean = dataset.dropna()
+print(dataset_clean.head())
 dataset = dataset.drop_duplicates()
 dataset = dataset.reset_index(drop=True)
-dataset.to_csv('data/Haowei_cleaned.csv')
+dataset_clean.to_csv('data/Haowei_cleaned.csv')
+
+
 
 
 
