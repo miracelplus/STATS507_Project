0ef505a68f0a2c9d559f3c030d52fa08d675e979
Leeeric2007 <128105113+Leeeric2007@users.noreply.github.com>
2023-03-21 22:26:58 -0400

    Add files via upload

---
+++ analysis_algorithm/tp_test3.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+
+
+data1 = pd.read_csv("ICE_MARRY_INCOME_EDU.csv", engine='python',on_bad_lines='skip')
+#data1.columns =['CASEID','ICE', 'GOVT','INCOME','AGE','SEX' ,'MARRY', 'EDUC']
+data1['INCOME'] = pd.to_numeric(data1['INCOME'], errors='coerce')
+data1['INCOME2']=data1.loc[:,['INCOME']].round(-4)
+data1['INCOME2']=data1.loc[:,['INCOME2']].clip(upper=100000.0)
+print(data1.iloc[[4]])
+#print(data1.dtypes)
+#,col_order=['0.0','10000.0','20000.0','30000.0','40000.0','50000.0','60000.0','70000.0','80000','90000.0','100000.0']
+grid = sns.FacetGrid(data1[['MARRY', 'INCOME2']],col='INCOME2',col_wrap=4,col_order=[0.0,10000.0,20000.0,30000.0,40000.0,50000.0,60000.0,70000.0,80000.0,90000.0,100000.0])
+_=grid.map_dataframe(sns.histplot,x='MARRY',stat='percent',binwidth=0.5)
+
+plt.figure(1)
+
+grid = sns.FacetGrid(data1[['INCOME2', 'EDUC']],col='INCOME2',col_wrap=4,col_order=[0.0,10000.0,20000.0,30000.0,40000.0,50000.0,60000.0,70000.0,80000.0,90000.0,100000.0])
+_=grid.map_dataframe(sns.histplot,x='EDUC',stat='percent',binwidth=0.5)
+
+plt.figure(2)
+
+grid = sns.FacetGrid(data1[['INCOME2', 'SEX']],col='INCOME2',col_wrap=4,col_order=[0.0,10000.0,20000.0,30000.0,40000.0,50000.0,60000.0,70000.0,80000.0,90000.0,100000.0])
+_=grid.map_dataframe(sns.histplot,x='SEX',stat='percent',binwidth=0.5)
+#for axes in grid.axes.flat:
+#    _ = axes.set_xticklabels(axes.get_xticklabels(), rotation=90)
+plt.figure(3)
+
+plt.figure(4)
+data1[['INCOME2']].value_counts().loc[[0.0,10000.0,20000.0,30000.0,40000.0,50000.0,60000.0,70000.0,80000.0,90000.0,100000.0]].plot.bar()
+#plt.hist(data1[['INCOME2']],bins=[10000,20000])
+
+
+
+plt.show()
