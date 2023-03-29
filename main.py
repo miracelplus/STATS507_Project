2fcbbaf2e205d1e97475dbb71cb666f271000f07
FrancisJian1999 <118216686+FrancisJian1999@users.noreply.github.com>
2023-03-28 21:15:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -652,8 +652,9 @@ app.layout = html.Div([
 
     html.Div([
         html.H2('Final Conclusion', style={'color': 'blue'}),
-        html.P('''The prediction result shows that linear regression maintains a higher performance than linear regression model. However, 
-        with larger sample sizes, RNN model seems to have better performance compared with linear regression model. ''', style={'fontSize': 18})
+        html.P('''The prediction result shows that linear regression model maintains a higher performance than random forest model and XG boost model. However, 
+        with larger sample sizes, RNN model seems to have better performance compared with linear regression model. The machine learning model is able to 
+        predict general upward trend of GDP increase for the US. However, the temporary recession caused by events like covid-19 is still unpredictable.''', style={'fontSize': 18})
     ]),
 
 ])
